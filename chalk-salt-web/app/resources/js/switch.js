7034537492e3b19569ef075c8c1b7ae77e7665ac
Abhishek <Abhishek@Dell>
2015-12-25 17:20:47 +0530

    Simple layout profile page setting of student.

---
+++ chalk-salt-web/app/resources/js/switch.js
@@ -0,0 +1,36 @@
+$(document).ready(function() { 
+	$(".blue").click(function(){
+      $("link").attr("href", "css/chalk-Dust.css");
+      return false;
+   });
+   
+   $(".yellow").click(function(){
+      $("link").attr("href", "css/chalk-Dust.css");
+      return false;
+   });
+   
+   $(".green").click(function(){
+      $("link").attr("href", "css/chalk-Dust.css");
+      return false;
+   });
+
+   $(".red").click(function(){
+      $("link").attr("href", "css/chalk-Dust.css");
+      return false;
+   });
+
+   $(".black").click(function(){
+      $("link").attr("href", "css/chalk-Dust.css");
+      return false;
+   });
+
+   $(".sweet").click(function(){
+      $("link").attr("href", "css/chalk-Dust.css");
+      return false;
+   });
+
+   $(".orange").click(function(){
+      $("link").attr("href", "css/chalk-Dust.css");
+      return false;
+   });
+});
