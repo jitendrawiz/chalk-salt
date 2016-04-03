35244ceeba83ce323fa8f2369a04029ee1dd1d83
Abhishek Kumar <abhishek.kumar627@gmail.com>
2016-04-03 18:00:53 +0530

    Design changes 1. Replace Chalkdust css with individual css in whole website.

--- chalk-salt-web/app/resources/js/switch.js
+++ chalk-salt-web/app/resources/js/switch.js
@@ -1,36 +1,36 @@
 $(document).ready(function() { 
 	$(".blue").click(function(){
-      $("link").attr("href", "css/chalk-Dust.css");
+      $("link").attr("href", "css/profileDetails/blue.css");
       return false;
    });
    
    $(".yellow").click(function(){
-      $("link").attr("href", "css/chalk-Dust.css");
+      $("link").attr("href", "css/profileDetails/yellow.css");
       return false;
    });
    
    $(".green").click(function(){
-      $("link").attr("href", "css/chalk-Dust.css");
+      $("link").attr("href", "css/profileDetails/green.css");
       return false;
    });
 
    $(".red").click(function(){
-      $("link").attr("href", "css/chalk-Dust.css");
+      $("link").attr("href", "css/profileDetails/red.css");
       return false;
    });
 
    $(".black").click(function(){
-      $("link").attr("href", "css/chalk-Dust.css");
+      $("link").attr("href", "css/profileDetails/black.css");
       return false;
    });
 
    $(".sweet").click(function(){
-      $("link").attr("href", "css/chalk-Dust.css");
+      $("link").attr("href", "css/profileDetails/sweet.css");
       return false;
    });
 
    $(".orange").click(function(){
-      $("link").attr("href", "css/chalk-Dust.css");
+      $("link").attr("href", "css/profileDetails/orange.css");
       return false;
    });
 });
